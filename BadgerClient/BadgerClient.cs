da1d206125ff47811da039f2c8585ef6dbec2e89
Badger <unconfigured@null.spigotmc.org>
2020-12-16 23:38:17 +0000

    Add project files.

---
+++ BadgerClient/BadgerClient.cs
@@ -0,0 +1,52 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using System;
+using System.Collections.Generic;
+using static CitizenFX.Core.Native.API;
+
+namespace BadgerClient
+{
+    public class BadgerClient : BaseScript
+    {
+        public BadgerClient()
+		{
+            EventHandlers["BadgerCopyOutfit:GetOutfit"] += new Action<int>(GetOutfit);
+            EventHandlers["BadgerCopyOutfit:SetOutfit"] += new Action<IList<dynamic>>(SetOutfit);
+
+            TriggerEvent("chat:addSuggestion", "/copyoutfit", "Copies the outfit of another player", new[]
+            {
+                new { name = "PlayerID", help = "ID of the player whose outfit you want to copy" }
+            });
+        }
+
+        // Get current outfit and send to server
+        private void GetOutfit(int playerToGiveOutfit)
+		{
+            int ped = PlayerPedId();
+            IList<dynamic> outfit = new List<dynamic>();
+
+            // Get outfit components
+            for (int i = 1; i <= 11; i++)
+            {
+                int component = GetPedDrawableVariation(ped, i);
+                outfit.Add(component);
+            }
+
+            // Send outfit back to server
+            TriggerServerEvent("BadgerCopyOutfit:SendOutfitToServer", outfit, playerToGiveOutfit);
+        }
+
+        // Set outfit
+        private void SetOutfit(IList<dynamic> outfit)
+		{
+            int ped = PlayerPedId();
+
+            for (int i = 1; i <= 11; i++)
+			{
+                SetPedComponentVariation(ped, i, outfit[i-1], 1, 0);
+			}
+
+            Screen.ShowNotification("~y~[BadgerCopyOutfit]\n~w~Outfit successfully copied");
+		}
+    }
+}
---
+++ BadgerServer/BadgerServer.cs
@@ -0,0 +1,39 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using static CitizenFX.Core.Native.API;
+
+namespace BadgerServer
+{
+    public class BadgerServer : BaseScript
+    {
+        public BadgerServer()
+		{
+			// Get outfit from client
+			EventHandlers["BadgerCopyOutfit:SendOutfitToServer"] += new Action<IList<dynamic>, int>(SendOutfit);
+
+			RegisterCommand("copyOutfit", new Action<int, List<object>, string>((source, args, raw) =>
+			{
+				if (args.Count > 0)
+				{
+					if (IsPlayerAceAllowed(source.ToString(), "BadgerCopyOutfit.Command"))
+					{
+						Player targetPlayer = Players[int.Parse(args[0].ToString())];
+
+						// Get outfit from target player
+						TriggerClientEvent(targetPlayer, "BadgerCopyOutfit:GetOutfit", source);
+					}
+				}
+				else
+					Debug.WriteLine("BadgerCopyOutfit: No command args");
+			}), false);
+		}
+
+		// Send outfit to client
+		private void SendOutfit(IList<dynamic> outfit, int targetPlayer)
+		{
+			TriggerClientEvent(Players[targetPlayer], "BadgerCopyOutfit:SetOutfit", outfit);
+		}
+    }
+
+}
